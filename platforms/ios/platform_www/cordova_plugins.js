0724c74d42dd7c296e8ec0ae32a0f3828f3b6d79
halachev <halachev-mac@Halachev-2.local>
2017-02-19 16:56:33 +0000

    init

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,48 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "id": "cordova-plugin-vibration.notification",
+        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
+        "pluginId": "cordova-plugin-vibration",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    },
+    {
+        "id": "cordova-plugin-progress.Progress",
+        "file": "plugins/cordova-plugin-progress/www/progress.js",
+        "pluginId": "cordova-plugin-progress",
+        "clobbers": [
+            "progress"
+        ]
+    },
+    {
+        "id": "cordova-plugin-admobpro.AdMob",
+        "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
+        "pluginId": "cordova-plugin-admobpro",
+        "clobbers": [
+            "window.AdMob"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.1",
+    "cordova-plugin-splashscreen": "4.0.1",
+    "cordova-plugin-vibration": "2.1.3",
+    "cordova-plugin-progress": "0.3.0",
+    "cordova-plugin-extension": "1.5.1",
+    "cordova-plugin-admobpro": "2.26.0"
+};
+// BOTTOM OF METADATA
+});
